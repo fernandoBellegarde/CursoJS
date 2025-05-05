856e924381e36833054a51f986586bc88660dcf8
fernandoBellegarde <bellegardefernando@gmail.com>
2025-05-05 15:54:17 -0300

    O começo

--- aula005/app.js
+++ aula005/app.js
@@ -1,1 +1,29 @@
-alert('Hello World!');
+let nomeCompleto = "Caio";
+let idade = 18;
+// let cria uma variavel que pode mudar
+
+const a = "a";
+// const cria uma variavel que é constante
+
+console.log(nomeCompleto);
+// console printa
+
+let nome = prompt("Qual é o seu nome?", "Insira aqui seu nome");
+// let resposta = confirm()
+
+alert(`Olá ${nome}, tudo bem?`);
+// console.log(resposta)
+
+let numero1, numero2, soma;
+
+numero1 = parseInt(prompt("digite o numero 1 para a soma"));
+numero2 = parseInt(prompt("digite o numero 2 para a soma"));
+/* tratamento: 
+´parseInt´ = inteiro 
+´Number´ = inteiro ou decimal
+´parseFloat´ = decimal
+*/
+soma = numero1 + numero2;
+
+alert(`soma = ${soma}`);
+// alert(String(soma)) converte pra String dnv
