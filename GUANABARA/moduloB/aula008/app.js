bdffef9a87863a9d038c89afa293582fd06a0959
fernandoBellegarde <bellegardefernando@gmail.com>
2025-05-10 15:22:49 -0300

    modulo B completo

---
+++ GUANABARA/moduloB/aula008/app.js
@@ -0,0 +1,34 @@
+/* 
+    Operadores de comparação
+    
+    "=" é uma atribuição
+    "==" é uma comparação de valor
+    "===" é uma comparação de valor e tipo
+*/
+
+5 > 2; // true
+5 < 2; // false
+5 >= 2; // true
+5 <= 2; // false
+5 == 2; // false
+5 != 2; // true
+5 === 2; // false
+5 !== 2; // true
+5 == '5'; // true
+5 === '5'; // false
+5 != '5'; // false
+5 !== '5'; // true
+
+// Operadores lógicos
+// && = E
+// || = OU
+// ! = NÃO
+
+/* 
+    Operador ternário
+    (condição) ? valor1 : valor2
+    Se a condição for verdadeira, retorna o valor1, caso contrário retorna o valor2
+*/
+
+media >= 7.0 ? 'Aprovado' : 'Reprovado'; 
+// Se a média for maior ou igual a 7.0, retorna 'Aprovado', caso contrário retorna 'Reprovado'
