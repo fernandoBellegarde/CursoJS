2bd76074302d43c0b7f0b97123a0f06c6d7f7d51
fernandoBellegarde <bellegardefernando@gmail.com>
2025-05-26 15:22:57 -0300

    estetica ex 4 guanabara

--- GUANABARA/Exercicios/EX004/app.js
+++ GUANABARA/Exercicios/EX004/app.js
@@ -12,17 +12,7 @@ function tabuada() {
       resultado = num * i;
       let item = document.createElement("option");
       item.text = `${num} x ${i} = ${resultado}`;
-      item.value = `select${c}`;
       select.appendChild(item);
     }
   }
-
-  /*
-  7 x 1 = 7
-  7 x 2 = 14 
-  7 x 3 = 21
-  7 x 4 = 28
-  7 x 5 = 35
-  7 x 6  =42
-  */
 }
