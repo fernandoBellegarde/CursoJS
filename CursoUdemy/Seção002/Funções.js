a3ffdd545d53e6d1cb950f817b4e171a70dd0c54
fernandoBellegarde <bellegardefernando@gmail.com>
2025-06-07 14:40:58 -0300

    Funções basico

---
+++ CursoUdemy/Seção002/Funções.js
@@ -0,0 +1,13 @@
+function soma(x, y) {
+  const resultado = x + y;
+  return resultado;
+}
+
+console.log(soma(1, 3));
+console.log(soma(10, 5));
+console.log(soma(5, 8));
+console.log("---------------");
+
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(9));
