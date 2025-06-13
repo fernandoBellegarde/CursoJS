b151916eac11d82b1fb93574530ba2c696d97eec
fernandoBellegarde <bellegardefernando@gmail.com>
2025-06-13 12:49:47 -0300

    feat: add error handling with try-catch in retornaHora function and log output

--- CursoUdemy/Seção003/tryCatch.js
+++ CursoUdemy/Seção003/tryCatch.js
@@ -1,4 +1,18 @@
 try {
+  // É executado quando não tem erros
+  console.log("Abri um arquivo");
+  console.log("Manipulei o arquivo e gerou erro");
+  console.log("fechei o arquivo"); // Não fecha pq pulou ja que gerou erro
+} catch (e) {
+  // É executado quando há erros
+  console.log("Tratando o erro");
+} finally {
+  // Sempre
+  console.log("FINALLY: Eu sempre sou executado, fechar arquivo");
+}
+console.log("-----------------");
+
+try {
   console.log(NãoExisto);
 } catch (err) {
   console.log("NãoExisto não existe");
