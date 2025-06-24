98287f8e4257bbf8f8dbc3795a1d2e2597959123
fernandoBellegarde <bellegardefernando@gmail.com>
2025-06-24 14:39:23 -0300

    basiquinho do escopo lexico

---
+++ CursoUdemy/Seção004/escopoLexico.js
@@ -0,0 +1,12 @@
+const nome = "Nando";
+
+function falaNome() {
+  // const nome = 'Bell'
+  console.log(nome);
+}
+
+function usaFalaNome() {
+  const nome = "Bell";
+  falaNome();
+}
+usaFalaNome();
