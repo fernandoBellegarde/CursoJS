fc711676185794581829f7834f82cc181df3889b
fernandoBellegarde <bellegardefernando@gmail.com>
2025-07-02 17:28:23 -0300

    focus apenas so pra ficar mais legal

--- CursoUdemy/Seção004/Exercicio/Calculadora copy/assets/js/main.js
+++ CursoUdemy/Seção004/Exercicio/Calculadora copy/assets/js/main.js
@@ -24,8 +24,10 @@ function Calculadora() {
     });
   };
 
-  this.addNumDisplay = (el) => (this.display.value += el.innerText);
-
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+    this.display.focus();
+  };
   this.clear = () => (this.display.value = " ");
 
   this.del = () => (this.display.value = this.display.value.slice(0, -1));
